6e93c15a9d6e3814acd384d7ff88df4dfd7e8d13
Anshi Sachan <anshijio123@gmail.com>
2024-05-24 14:36:20 +0530

    Assets:Rename ESIC logo

--- esic_fe/src/components/Loading/Loading.jsx
+++ esic_fe/src/components/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import union from '../../assets/union.png'
+import esic_logo from '../../assets/esic_logo.png'
 
 function Loading() {
   return (
@@ -12,7 +12,7 @@ function Loading() {
 
         {/* Content block */}
         <div className=' w-80 h-80 text-center flex flex-col justify-center items-center'>
-            <img className='w-20 h-28 my-3' src={union} alt='E-SUMMIT Logo'/>
+            <img className='w-20 h-28 my-3' src={esic_logo} alt='E-SUMMIT Logo'/>
             <p className='text-gray-800 font-sans font-bold text-3xl py-2'>E-SUMMIT</p>
             <p className='text-gray-700 font-sans font-normal text-lg'>& Innovation<br/>Committee</p>
         </div>
