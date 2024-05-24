232feb71dc3f74e95130af623379ca10b31a99e7
marcdhi <gandhi.mardav@gmail.com>
2024-05-24 22:20:31 +0530

    marque: add aceternity base component for use

---
+++ esic_fe/src/UI/infinite_horizontal_moving.jsx
@@ -0,0 +1,88 @@
+import { cn } from "@/lib/utils";
+import React, { useEffect, useState } from "react";
+
+
+// the design here needs to be changed, make sure to change the design to fit the design of the website we are working on, try playing around with the tailwind classes to see what works for you and what doesn't, soon you will get the hang of it
+
+export const InfiniteMovingComponent = ({
+  text,
+  direction = "left",
+  speed = "fast",
+  pauseOnHover = true,
+  className,
+}) => {
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const scrollerRef = React.useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    addAnimation();
+  }, []);
+  const [start, setStart] = useState(false);
+  function addAnimation() {
+    if (containerRef.current && scrollerRef.current) {
+      const scrollerContent = Array.from(scrollerRef.current.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        if (scrollerRef.current) {
+          scrollerRef.current.appendChild(duplicatedItem);
+        }
+      });
+
+      getDirection();
+      getSpeed();
+      setStart(true);
+    }
+  }
+  const getDirection = () => {
+    if (containerRef.current) {
+      if (direction === "left") {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "forwards"
+        );
+      } else {
+        containerRef.current.style.setProperty(
+          "--animation-direction",
+          "reverse"
+        );
+      }
+    }
+  };
+  const getSpeed = () => {
+    if (containerRef.current) {
+      if (speed === "fast") {
+        containerRef.current.style.setProperty("--animation-duration", "20s");
+      } else if (speed === "normal") {
+        containerRef.current.style.setProperty("--animation-duration", "40s");
+      } else {
+        containerRef.current.style.setProperty("--animation-duration", "80s");
+      }
+    }
+  };
+  return (
+    <div
+      ref={containerRef}
+      className={cn(
+        "scroller relative z-20  max-w-7xl overflow-hidden  [mask-image:linear-gradient(to_right,transparent,white_20%,white_80%,transparent)]",
+        className
+      )}
+    >
+      <ul
+        ref={scrollerRef}
+        className={cn(
+          " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
+          start && "animate-scroll ",
+          pauseOnHover && "hover:[animation-play-state:paused]"
+        )}
+      >
+
+        {/* this is where the text will be displayed, which will be passed in as a prop from the MarqueScroll component */}
+
+        {text}
+
+        
+      </ul>
+    </div>
+  );
+};
--- esic_fe/src/components/Marque/Marque.jsx
+++ esic_fe/src/components/Marque/Marque.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { InfiniteMovingComponent } from "@/UI/infinite_horizontal_moving";
 
-export function MarqueScroll() {
+export function MarqueScroll({text}) {
     return (
         <div className="text-secondary-900">
-            Marque Scroll use from Aceternity UI
+
+            Use the InfiniteMovingComponent to create a Marque Scroll
+
+            {/* this is how you use the InfiniteMovingComponent
+            <InfiniteMovingComponent text={"some text here that the MarqueScroll component takes in"}/> */}
+                
+            Other sections would use this component to create a Marque Scroll
+            they will pass in the items text only for now
+
+            {/* This is a sample of how others would use this component as 
+            <MarqueScroll text={"some text here"} /> */}
+
+            You will have to modify stuff in the InfiniteMovingComponent to make it work for you
+
+            the design has to be changed to fit the design of the website you are working on
+
         </div>
     );
     }
