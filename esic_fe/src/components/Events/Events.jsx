ab68c7c0fc5c5af0c889d26f0b79b25a0eff838e
alphaplayerofdooms <154459496+alphaplayerofdooms@users.noreply.github.com>
2024-05-25 19:04:50 +0530

    basic part completed

--- esic_fe/src/components/Events/Events.jsx
+++ esic_fe/src/components/Events/Events.jsx
@@ -1,9 +1,12 @@
 import React from "react";
-
+import Carousel from "./Carousel";
+import "./Carousel.css";
 export function Events() {
     return (
-        <div className="text-secondary-900">
-            Events
+        <div className="text-secondary-900 bg-hero-50 h-auto w-auto outerdiv">
+            <p className="flex justify-center text-7xl text-white">Events</p>
+            <Carousel/>
+            <Carousel/>
         </div>
     );
     }
