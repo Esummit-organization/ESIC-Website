d7358990ce592c88ed9137c24c1d322d7ab0a927
mohnishraj <mohnishrajmani317@gmail.com>
2024-06-16 09:16:54 +0530

    blog list : improved BlogList grid, improved bloglist card, added a horizontal prop to handle featured card, created page title component

--- esic_fe/src/Pages/Blogs/Blogs.jsx
+++ esic_fe/src/Pages/Blogs/Blogs.jsx
@@ -1,10 +1,49 @@
 import React from "react";
-import { BlogList } from "@/components/Blog/BlogList";
+import BlogListGrid from "@/components/Blog/BlogListGrid";
+import BlogListCard from "@/components/Blog/BlogListCard";
+import { Link } from "react-router-dom";
+
+function BlogPageTitle({ children }) {
+  return (
+    <h1
+      className="p-8 bg-blog-50 text-xl tracking-wider relative hover:tracking-widest cursor-pointer
+                transition-all hover:text-sponsor-50
+                before:content-normal before:absolute before:h-1
+                before:bg-white before:w-3 before:top-2/3 hover:before:w-10
+                hover:before:bg-sponsor-50 before:transition-all"
+    >
+      {children}
+    </h1>
+  );
+}
 
 export function Blogs() {
-    return (
-        <div>
-            <BlogList />
-        </div>
-    );
-    }
+  const featuredCard = {
+    image_url:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSujiuIp40oRg8BHrZPURhWblHvAX4XKMcVxA&s",
+    date: "11 July 2024",
+    blog_title: "Scaling up",
+    blog_content:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate itaque omnis est molestias provident, nesciunt soluta vitae, quia earum maxime sapiente repellat debitis. Qui eum delectus eveniet molestias voluptates laudantium.",
+    tags: ["Entrepreneurship", "startup"],
+  };
+
+  return (
+    <>
+      <BlogPageTitle>FEATURED</BlogPageTitle>
+      <div className="px-8 bg-blog-50">
+        <BlogListCard {...featuredCard} horizontal />
+      </div>
+      <BlogPageTitle>Recent Blogs</BlogPageTitle>
+      <BlogListGrid />
+      <div className="grid place-items-center p-8 bg-gradient bg-gradient-to-tr from-blog-50 to-blog-100">
+        <Link
+          to="#"
+          className="text-sm border p-4 hover:bg-white hover:text-blog-50 transition"
+        >
+          Load More...
+        </Link>
+      </div>
+    </>
+  );
+}
--- esic_fe/src/components/Blog/BlogListCard.jsx
+++ esic_fe/src/components/Blog/BlogListCard.jsx
@@ -1,40 +1,52 @@
-import React from 'react'
+import { cn } from "@/lib/utils";
+import React from "react";
 
-const BlogListCard = ({
-    image_url='',
-    date='',
-    blog_title='ESIC',
-    blog_content='E-summit NITK blogs',
-    tags=['']})   =>   {
-
-        let tagElements = [];
-        for(let i=0;i<tags.length;i++){
-            if(tags[i]!=''){
-                tagElements[i]=<div className='border-[1px] border-sponsor-50 rounded-xl p-[5px] ml-2'>
-                           <p className='text-[10px] md:text-[13px]'>{tags[i]}</p>
-                           </div>;
-            }      
-        }
-
-        return (
-    <div className='relative w-full p-0 sm:m-0 h-[350px] md:h-[520px] rounded-3xl border-2 border-white bg-gradient-to-br from-blog-50 to-hero-50 flex flex-col'>
-        <div className='relative w-full h-1/2 flex items-center justify-center rounded-t-3xl'>
-            <img className='absolute w-full h-full object-cover rounded-t-3xl' alt="blog-img" src={image_url}></img>
-        </div>
-        <div className='border-b-2 border-t-2 border-white'>
-            <h2 className='ml-3 text-[17px] md:text-[20px]'>{date}</h2>
-        </div>
-        <div className='flex flex-col mt-2 mb-3'>
-            <div className='text-center text-[17px] md:text-[22px]'><h2>{blog_title}</h2></div>
-            <div className='mx-3'>
-                <p className='text-[14px] md:text-[17px] text-justify'>{blog_content}</p>
-            </div>
-        </div>
-        <div className='flex mx-2 mb-2'>
-            {tagElements}
-        </div>
-    </div>  
-  )
+function TagElement({ children }) {
+  return (
+    <p className="border border-sponsor-50 rounded-xl p-1 text-xs inline-block">
+      {children}
+    </p>
+  );
 }
 
-export default BlogListCard
+const BlogListCard = ({
+  image_url = "",
+  date = "",
+  blog_title = "ESIC",
+  blog_content = "E-summit NITK blogs",
+  tags = [""],
+  horizontal = false,
+}) => {
+  const tagElements = tags.map((tag) => (
+    <TagElement key={tag}>{tag}</TagElement>
+  ));
+
+  return (
+    <div
+      className={cn(
+        "sm:m-0 overflow-hidden rounded-3xl border-2 border-white bg-gradient-to-br from-blog-50 to-hero-50 flex flex-col",
+        horizontal && "grid grid-cols-2 max-sm:flex"
+      )}
+    >
+      <figure className={cn(horizontal && "h-full")}>
+        <img
+          className="w-full h-full object-cover"
+          src={image_url}
+          alt={blog_title}
+        />
+      </figure>
+
+      <div className="flex flex-col">
+        <p className="px-4 sm:px-8 py-1">{date}</p>
+        <hr />
+        <h1 className="text-center text-xl py-2">{blog_title}</h1>
+        <p className="line-clamp-4 text-ellipsis px-4 sm:px-8 grow text-justify">
+          {blog_content}
+        </p>
+        <div className="py-4 px-4 sm:px-8 space-x-2">{tagElements}</div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogListCard;
