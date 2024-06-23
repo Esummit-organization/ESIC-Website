429cab351fe7947722f3beb12b9069d822a7868d
Anshi Sachan <anshijio123@gmail.com>
2024-06-23 17:40:03 +0530

    faq: add FAQs section

---
+++ esic_fe/src/constants/faq.jsx
@@ -0,0 +1,22 @@
+export const QUESTIONS = [
+    {
+      question: 'Why is Raycast free for personal use?',
+      answer: 'Answer 1',
+    },
+    {
+      question: 'When is Raycast for teams available?',
+      answer: 'Answer 2',
+    },
+    {
+      question: 'How many seats do I get in a Team plan?',
+      answer: 'Answer 3',
+    },
+    {
+      question: 'Can I have personal Extensions and Team Extensions?',
+      answer: 'Answer 4',
+    },
+    {
+      question: 'How much will Team features cost?',
+      answer: 'Answer 5',
+    },
+  ];
