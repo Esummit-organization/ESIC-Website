339102b7bb34e994d51aadba56a93c138dcba9dd
Anshi Sachan <anshijio123@gmail.com>
2024-06-23 17:45:52 +0530

    faq: change font size

--- esic_fe/src/components/FAQ/FAQ.jsx
+++ esic_fe/src/components/FAQ/FAQ.jsx
@@ -17,7 +17,7 @@ export function FAQ() {
         <div className="bg-secondary-950 p-10 pt-6 md:p-20 lg:p-28">
             <div className="max-w-full mx-auto p-4 pt-6 md:p-6 lg:p-12 bg-background-950 rounded shadow-md">
                 <h2 className="text-4xl font-bold mb-4 grotesk text-white">FAQs</h2>
-                <p className="poppins text-[11px] md:text-xl my-1 mb-14 text-[#0063c0]">Answers to the most frequently asked questions.</p>
+                <p className="poppins text-sm md:text-xl my-1 mb-14 text-[#0063c0]">Answers to the most frequently asked questions.</p>
                 {QUESTIONS.map((question, index) => (
                     <div key={index} className={`mb-4 ${index!== QUESTIONS.length - 1? 'border-b border-[#013769] pb-4' : ''}`}>
                         <div className="flex justify-between items-center cursor-pointer" onClick={() => toggleDropdown(index)}>
