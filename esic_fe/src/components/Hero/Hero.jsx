c5510897e012530486886333275e9bca6b59245d
sahil-mengji <mengjisahil@gmail.com>
2024-07-16 19:57:26 +0530

    improved responsivness and animation of the hero section parallax

--- esic_fe/src/components/Hero/Hero.jsx
+++ esic_fe/src/components/Hero/Hero.jsx
@@ -1,115 +1,170 @@
 import React from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
 import "./hero.css";
+import Plx from "react-plx";
 export function Hero() {
+	const parrallaxDivStyle =
+		"absolute left-0 top-0   w-full flex justify-center items-end h-[100vh] lg:h-[120vh] ";
+	const parallaxImageStyle = "  mx-auto max-h-[100vh] hover:scale-1.2";
+	const backgroundStyle = "w-full mx-auto h-[100vh] lg:h-[140vh]";
 	return (
-		<div className="  w-full bg-background-150 h-[100vh] hide-scrollbar">
-			<Parallax
-				pages={2}
-				style={{ top: "0", left: "0" }}
-				className="hide-scrollbar"
+		<div className="relative  w-full h-[100vh] lg:h-[120vh] bg-background-500 hide-scrollbar z-[5] overflow-hidden ">
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 1,
+								property: "scale",
+							},
+						],
+					},
+				]}
 			>
-				<ParallaxLayer offset={0} speed={0.25}>
-					<div className="flex justify-center items-end  h-[100vh]">
-						<img className=" w-full  h-full  " src="hero/1.png" alt="" />{" "}
-					</div>
-				</ParallaxLayer>
-				<ParallaxLayer offset={0} speed={-0.01}>
-					<div className="flex justify-center items-end  h-[100vh]">
-						<img
-							style={{ aspectRatio: 1.15 }}
-							className="w-full  max-h-[100vh] h-auto max-w-[100vh] "
-							src="hero/2.png"
-							alt=""
-						/>{" "}
-					</div>
-				</ParallaxLayer>
-				<ParallaxLayer offset={0} speed={-0.05}>
-					<div className="flex justify-center items-end  h-[100vh]">
-						<img
-							style={{ aspectRatio: 1.15 }}
-							className="w-full  max-h-[100vh] h-auto max-w-[100vh] "
-							src="hero/3.png"
-							alt=""
-						/>{" "}
-					</div>
-				</ParallaxLayer>
-				<ParallaxLayer offset={0} speed={0.5}>
-					<div className="flex justify-center items-end  h-[100vh]">
-						<img
-							style={{ aspectRatio: 1.15 }}
-							className="w-full  max-h-[100vh] h-auto max-w-[100vh]"
-							src="hero/4.png"
-							alt=""
-						/>{" "}
-					</div>
-				</ParallaxLayer>
-				<ParallaxLayer offset={0} speed={0.35}>
-					<div className="flex justify-center items-end  h-[100vh]">
-						<img className="w-full max-h-[87vh] " src="hero/5.png" alt="" />{" "}
-					</div>
-				</ParallaxLayer>
-				<ParallaxLayer offset={0} speed={0.5}>
-					<div className="flex justify-center items-end  h-[100vh] w-full ">
-						<img
-							style={{ aspectRatio: 1.15 }}
-							className="w-full max-h-[100vh] max-w-[100vh]"
-							src="hero/6.png"
-							alt=""
-						/>{" "}
-					</div>
-				</ParallaxLayer>
-				{/* <ParallaxLayer offset={0} speed={0.5}>
-					<div className="flex justify-center items-end  h-[100vh] w-full relative ">
-						<div className="absolute bottom-0 left-0 w-[110%] h-[80px] bg-background-500 rotate-2 border border-2 border-solid border-[#000000]">
-							<p className="w-full  h-full flex items-center text-3xl text-[#000000] tracking-widest justify-center items-end text-nowrap">
-								Startups • Networking • Enterprunership • Startups • Networking
-								• Enterprunership • Networking • Enterprunership • Startups •
-								Networking • Enterprunership
-							</p>
-						</div>
-					</div>
-				</ParallaxLayer> */}
-				<ParallaxLayer offset={0} speed={0.5}>
-					<div className="flex justify-center items-end  h-[100vh] w-full ">
-						<img className="h-[100vh] w-full  " src="hero/7.png" alt="" />{" "}
-					</div>
-				</ParallaxLayer>
-				<ParallaxLayer offset={1} speed={0.35}>
-					<div className="flex justify-center items-end  h-[100vh] w-full  relative">
-						<div className="absolute top-[-280px] left-0 w-[110%] h-[80px] bg-background-500 rotate-2 border-2 border-solid border-[#000000]">
-							<p className="grotesk w-full  h-full flex items-center text-3xl text-[#000000] tracking-widest justify-center text-nowrap">
-								Startups • Networking • Enterprunership • Startups • Networking
-								• Enterprunership • Networking • Enterprunership • Startups •
-								Networking • Enterprunership
-							</p>
-						</div>
-					</div>
-				</ParallaxLayer>
-
-				<ParallaxLayer offset={1} speed={0}>
-					<div className="flex justify-center items-center h-[100vh] w-full  relative flex-col gap-[40px]">
-						<p className="grotesk text-4xl text-[#870000] font-semibold">
-							What is E-Summit ?
-						</p>
-						<p className="poppins max-w-[700px] text-center text-[#000000] text-lg">
-							{" "}
-							E-Summit is a three day event, started as an initiative of the
-							E-Cell. The Summit seeks to facilitate interaction among
-							industrialists, funding agencies, seasoned and upcoming
-							entrepreneurs, academia and students of the participating
-							institutes. E-Summit’24 will include a gamut of activities such as
-							entrepreneurial guest lectures, workshops and panel discussions
-							with experts, startup internship fair and investors{" "}
-						</p>
-
-						<div
-							style={{ aspectRatio: "9/5" }}
-							className="w-full  max-w-[600px] mx-[20px] bg-background-800 mt-[60px]"
-						></div>
-					</div>
-				</ParallaxLayer>
-			</Parallax>
+				<img className={backgroundStyle} src="hero/1.png" alt="foreground" />
+			</Plx>
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 2.5,
+								property: "scale",
+							},
+						],
+					},
+				]}
+				className={parrallaxDivStyle}
+			>
+				<img
+					style={{ aspectRatio: 1.15 }}
+					className={parallaxImageStyle}
+					src="hero/2.png"
+					alt="foreground"
+				/>
+			</Plx>
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 1.8,
+								property: "scale",
+							},
+						],
+					},
+				]}
+				className={parrallaxDivStyle}
+			>
+				<img
+					style={{ aspectRatio: 1.15 }}
+					className={parallaxImageStyle}
+					src="hero/3.png"
+					alt="foreground"
+				/>
+			</Plx>
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 1.5,
+								property: "scale",
+							},
+						],
+					},
+				]}
+				className={parrallaxDivStyle}
+			>
+				<img
+					style={{ aspectRatio: 1.15 }}
+					className={parallaxImageStyle}
+					src="hero/4.png"
+					alt="foreground"
+				/>
+			</Plx>
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 1.2,
+								property: "scale",
+							},
+						],
+					},
+				]}
+				className={parrallaxDivStyle}
+			>
+				<img
+					style={{ maxWidth: "100%", width: "100%" }}
+					className={parallaxImageStyle}
+					src="hero/5.png"
+					alt="foreground"
+				/>
+			</Plx>
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 1.6,
+								property: "scale",
+							},
+						],
+					},
+				]}
+				className={parrallaxDivStyle}
+			>
+				<img
+					style={{ aspectRatio: 1.15 }}
+					className={parallaxImageStyle}
+					src="hero/6.png"
+					alt="foreground"
+				/>
+			</Plx>
+			<Plx
+				parallaxData={[
+					{
+						start: 0,
+						end: 700,
+						easing: "ease-in",
+						properties: [
+							{
+								startValue: 1,
+								endValue: 1,
+								property: "scale",
+							},
+						],
+					},
+				]}
+			>
+				<img src="hero/7.png" alt="foreground" className={backgroundStyle} />
+			</Plx>
 		</div>
 	);
 }
--- esic_fe/src/components/Navbar/Navbar.jsx
+++ esic_fe/src/components/Navbar/Navbar.jsx
@@ -1,61 +1,143 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-import {motion} from "framer-motion";
-import {options} from "@/constants/navbar";
+import { motion } from "framer-motion";
+import { options } from "@/constants/navbar";
 
 export function Navbar() {
-    
-    const [menu, setMenu] = useState(false)
-    
-    const showMenu = () =>{
-        setMenu((prev) => !prev)
-    }
-    const closeMenu = () =>{
-        setMenu(false)
-    }
-    
-        return (
-        <nav className={"duration-300 bg-background-950  text-secondary-900 w-screen fixed z-30"}>
-            <div className={`${menu ? "text-secondary-900 bg-background-950 w-screen fixed" :"" }`}>
-                <div className="flex place-content-between items-start md:items-center py-4 px-10">
-                    <div className="justify-self-start">
-                        <motion.div whileTap={{scale: 0.93}} initial={{x: -60}} animate={{x: 0}} transition={{duration: 0.6}}><Link to="/" className="flex items-start ">
-                            <svg width="28" height="41" viewBox="0 0 28 41" fill="none" xmlns="http://www.w3.org2000/svg">
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M8.08186 8.08186H0V0H8.08186H19.9467V8.08186H8.08186ZM0.0576284 16.1636L8.08186 24.1878V24.2454H0.0576298V32.3273H8.13949V24.2457H19.9467V16.1639H8.11083V16.1636H0.0576284ZM19.9467 32.3274H8.08186V32.3274H0.0576284L8.08186 40.3517V40.4092H19.9467V32.3274ZM19.9474 8.05327L28.0006 6.77109e-05L19.9474 6.84149e-05L19.9474 8.05327ZM28.0006 16.1636L19.9474 24.2168L19.9474 16.1636L28.0006 16.1636ZM19.9474 40.3806L28.0006 32.3274H19.9474L19.9474 40.3806ZM0.0285912 8.082L8.08179 16.1352V8.082H0.0285912Z" fill="white" fill-opacity="0.7"/>
-                            </svg>
-                            <div className="flex flex-col px-2">
-                                <div className="text-white font-bold">E-SUMMIT</div>
-                                <div className="text-white text-xs">& Innovation Committee</div>
-                            </div>
-                            </Link>
-                        </motion.div>
-                    </div>
-                    <motion.ul initial={{opacity: 0}} animate={{opacity: 1}} transition={{duration: 1}} className="hidden md:flex ">
-                        {options.map((options) => (
-                            <motion.li whileTap={{scale: 0.9}} className="m-2 cursor-pointer text-white text-xs hover:text-gray-300 duration-150">
-                            <NavLink to= {options.to} className={({isActive}) => `${isActive?"font-bold":""} p-2`}>{options.name}</NavLink>
-                            </motion.li>
-                        ))}
-                    </motion.ul>
-                    <div className="w-[13%]"></div>
-                    {/* <motion.div initial={{y: -70}} animate={{y: 0}} transition={{ type: "spring", bounce:0.5 }}>
+	const [menu, setMenu] = useState(false);
+
+	const [navBackground, setNavBackground] = useState(false);
+
+	const handleScroll = () => {
+		const show = window.scrollY > 80;
+		setNavBackground(show);
+	};
+
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	const showMenu = () => {
+		setMenu((prev) => !prev);
+	};
+	const closeMenu = () => {
+		setMenu(false);
+	};
+
+	return (
+		<nav
+			className={`${
+				navBackground ? "bg-background-950" : ""
+			} duration-300   text-secondary-900 w-screen fixed z-30`}
+		>
+			<div
+				className={`${
+					menu ? "text-secondary-900 bg-background-950 w-screen fixed" : ""
+				}`}
+			>
+				<div className="flex place-content-between items-start md:items-center py-4 px-10">
+					<div className="justify-self-start">
+						<motion.div
+							whileTap={{ scale: 0.93 }}
+							initial={{ x: -60 }}
+							animate={{ x: 0 }}
+							transition={{ duration: 0.6 }}
+						>
+							<Link to="/" className="flex items-start ">
+								<svg
+									width="28"
+									height="41"
+									viewBox="0 0 28 41"
+									fill="none"
+									xmlns="http://www.w3.org2000/svg"
+								>
+									<path
+										fill-rule="evenodd"
+										clip-rule="evenodd"
+										d="M8.08186 8.08186H0V0H8.08186H19.9467V8.08186H8.08186ZM0.0576284 16.1636L8.08186 24.1878V24.2454H0.0576298V32.3273H8.13949V24.2457H19.9467V16.1639H8.11083V16.1636H0.0576284ZM19.9467 32.3274H8.08186V32.3274H0.0576284L8.08186 40.3517V40.4092H19.9467V32.3274ZM19.9474 8.05327L28.0006 6.77109e-05L19.9474 6.84149e-05L19.9474 8.05327ZM28.0006 16.1636L19.9474 24.2168L19.9474 16.1636L28.0006 16.1636ZM19.9474 40.3806L28.0006 32.3274H19.9474L19.9474 40.3806ZM0.0285912 8.082L8.08179 16.1352V8.082H0.0285912Z"
+										fill="white"
+										fill-opacity="0.7"
+									/>
+								</svg>
+								<div className="flex flex-col px-2">
+									<div className="text-white font-bold">E-SUMMIT</div>
+									<div className="text-white text-xs">
+										& Innovation Committee
+									</div>
+								</div>
+							</Link>
+						</motion.div>
+					</div>
+					<motion.ul
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						transition={{ duration: 1 }}
+						className="hidden md:flex "
+					>
+						{options.map((options) => (
+							<motion.li
+								whileTap={{ scale: 0.9 }}
+								className="m-2 cursor-pointer text-white text-xs hover:text-gray-300 duration-150"
+							>
+								<NavLink
+									to={options.to}
+									className={({ isActive }) =>
+										`${isActive ? "font-bold" : ""} p-2`
+									}
+								>
+									{options.name}
+								</NavLink>
+							</motion.li>
+						))}
+					</motion.ul>
+					<div className="w-[13%]"></div>
+					{/* <motion.div initial={{y: -70}} animate={{y: 0}} transition={{ type: "spring", bounce:0.5 }}>
                         <motion.button whileTap={{scale: 0.75}} className="hidden md:block bg-gray-300 bg-opacity-35 rounded-full text-sm font-semibold text-white hover:bg-opacity-60 duration-300 pt-[3px] pb-[4px] px-1"><Link to="/login" className="p-3">LOGIN</Link></motion.button>
                     </motion.div> */}
-                    <div className="flex flex-col items-end md:hidden duration-200">
-                        <motion.button whileTap={{scale: 0.95}} whileHover={{scale: 1.15}} onClick={showMenu} className="flex md:hidden rounded-full"><img src={`${menu ?"https://freesvg.org/img/1544641784.png":"https://www.pngmart.com/files/23/Menu-Icon-PNG-Transparent.png"}`} alt={`${menu ?"close-icon":"menu-icon"}`} className={`${menu ?"size-6":"size-7"}`} /></motion.button>
-                        <motion.div layout >
-                            <ul className={menu ? "flex flex-col items-end " :"hidden"}>
-                                {options.map((options) => (
-                                    <motion.li whileTap={{scale: 0.9}} className="m-4 cursor-pointer text-white text-xs hover:text-gray-300 duration-150">
-                                    <NavLink to= {options.to} onClick={closeMenu} className={({isActive}) => `${isActive?"font-bold":""} p-2`}>{options.name}</NavLink>
-                                    </motion.li>
-                                ))}
-                                {/* <motion.li whileTap={{scale: 0.9}} className="m-2 cursor-pointer text-white text-xs hover:text-gray-300 duration-150 py-1"><Link to="/login" className="p-2">LOGIN</Link> </motion.li> */}
-                            </ul>
-                        </motion.div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
-    }
+					<div className="flex flex-col items-end md:hidden duration-200">
+						<motion.button
+							whileTap={{ scale: 0.95 }}
+							whileHover={{ scale: 1.15 }}
+							onClick={showMenu}
+							className="flex md:hidden rounded-full"
+						>
+							<img
+								src={`${
+									menu
+										? "https://freesvg.org/img/1544641784.png"
+										: "https://www.pngmart.com/files/23/Menu-Icon-PNG-Transparent.png"
+								}`}
+								alt={`${menu ? "close-icon" : "menu-icon"}`}
+								className={`${menu ? "size-6" : "size-7"}`}
+							/>
+						</motion.button>
+						<motion.div layout>
+							<ul className={menu ? "flex flex-col items-end " : "hidden"}>
+								{options.map((options) => (
+									<motion.li
+										whileTap={{ scale: 0.9 }}
+										className="m-4 cursor-pointer text-white text-xs hover:text-gray-300 duration-150"
+									>
+										<NavLink
+											to={options.to}
+											onClick={closeMenu}
+											className={({ isActive }) =>
+												`${isActive ? "font-bold" : ""} p-2`
+											}
+										>
+											{options.name}
+										</NavLink>
+									</motion.li>
+								))}
+								{/* <motion.li whileTap={{scale: 0.9}} className="m-2 cursor-pointer text-white text-xs hover:text-gray-300 duration-150 py-1"><Link to="/login" className="p-2">LOGIN</Link> </motion.li> */}
+							</ul>
+						</motion.div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
