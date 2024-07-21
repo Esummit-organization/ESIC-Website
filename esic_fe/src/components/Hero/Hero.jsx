5233775a8f6cd50451a0fd43e6f520a6cacb53ef
sahil-mengji <mengjisahil@gmail.com>
2024-07-22 02:51:08 +0530

    changed hero section color scheme

--- esic_fe/src/components/Hero/Hero.jsx
+++ esic_fe/src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ export function Hero() {
 	const parallaxImageStyle = "  mx-auto max-h-[100vh] hover:scale-1.2";
 	const backgroundStyle = "w-full mx-auto h-[100vh] lg:h-[140vh]";
 	return (
-		<div className="relative  w-full h-[100vh] lg:h-[120vh] bg-background-500 hide-scrollbar z-[5] overflow-hidden ">
+		<div className="hero-section-div  relative  w-full h-[100vh] lg:h-[120vh] bg-background-500 hide-scrollbar z-[5] overflow-hidden ">
 			<Plx
 				parallaxData={[
 					{
